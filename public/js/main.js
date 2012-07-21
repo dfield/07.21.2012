cc5d012dd6f9800ea392b22a58477fcfb7537a22
Tala Huhe <tala@fb.com>
2012-07-21 16:14:25 -0700

    Text

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,5 @@
+var gl = GL.create();
+
 $(document).ready(function() {
     var currentPage = "Potato";
     var relatedPages = [
@@ -11,15 +13,17 @@ $(document).ready(function() {
         [-10, 10],
     ];
 
-    var gl = GL.create();
     var cubeMesh = GL.Mesh.cube();
     var sphereMesh = GL.Mesh.sphere({
         detail: 6,
     });
+    var planeMesh = GL.Mesh.plane({
+        coords: true,
+    });
 
     var shader = new GL.Shader('\
 void main() {\
-gl_Position = gl_ModelViewProjectionMatrix * gl_Vertex;\
+  gl_Position = gl_ModelViewProjectionMatrix * gl_Vertex;\
 }\
 ', '\
 uniform vec3 color;\
@@ -28,9 +32,25 @@ void main() {\
 }\
 ');
 
+    var textureShader = new GL.Shader('\
+varying vec2 coord;\
+void main() {\
+  coord = gl_TexCoord.xy;\
+  gl_Position = gl_ModelViewProjectionMatrix * gl_Vertex;\
+}\
+', '\
+varying vec2 coord;\
+uniform sampler2D texture;\
+void main() {\
+  gl_FragColor = texture2D(texture, coord);\
+}\
+');
+
     gl.onupdate = function(seconds) {
 
     };
+
+    var test = new WikiPage('Plant');
 
     gl.ondraw = function() {
         gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
@@ -52,6 +72,15 @@ void main() {\
                 color: [0.5, 0.5, 0.5],
             });
             shader.draw(sphereMesh);
+
+            gl.translate(0, 1.5, 0);
+            gl.scale(1.5, 1.5, 1.5);
+            test.nameTexture.bind(0);
+            textureShader.uniforms({
+                texture: 0,
+            });
+            textureShader.draw(planeMesh);
+            test.nameTexture.unbind(0);
             gl.popMatrix();
         }
     };
