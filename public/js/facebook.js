8d9b25455d3ff357c611fc5c7867ca5efe2509a1
dfinzer <devin_finzer@brown.edu>
2012-07-21 14:44:36 -0700

    Beginning facebook

---
+++ public/js/facebook.js
@@ -0,0 +1,56 @@
+var uid = 0;
+var name = "Anonymous";
+
+function login(callback) {
+  FB.login(function(response) {
+    if (response.authResponse) {
+      uid = response.authResponse.userID;
+      $.cookie("uid", uid);
+      FB.api("/me", function(data) {
+        name = data.first_name;
+        callback();
+      });
+    } else {
+      //user cancelled login or did not grant authorization
+    }
+  }, {scope:'email'});
+}
+
+window.fbAsyncInit = function() {
+	var appId;
+	if(window.location.toString().indexOf('herokuapp') != -1) {
+		appId = '';//TODO make a new app for heroku.
+	} else {
+		appId = '346135092130975';
+	}
+  FB.init({ appId: appId, 
+	    status: true, 
+	    cookie: true,
+	    xfbml: true,
+	    oauth: true });
+
+  function updateButton(response) {
+    if (response.authResponse) {
+      console.log("Auth response.");
+    }
+  }
+
+  // run once with current status and whenever the status changes
+  FB.getLoginStatus(updateButton);
+  FB.Event.subscribe('auth.statusChange', updateButton);	
+};
+	
+(function() {
+  var e = document.createElement('script'); e.async = true;
+  e.src = document.location.protocol 
+    + '//connect.facebook.net/en_US/all.js';
+  document.getElementById('fb-root').appendChild(e);
+}());
+
+$(document).ready(function() {
+  $("#fb-login").click(function() {
+    login(function() {
+      alert("hello!")
+    });
+  });
+});
--- public/js/wiki.js
+++ public/js/wiki.js
@@ -1,5 +1,6 @@
+
+
 var socket = io.connect('http://localhost:7000');
 socket.on('news', function (data) {
-  console.log(data);
   socket.emit('my other event', { my: 'data' });
 });
