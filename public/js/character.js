c7b8a7ac1a47007a15bf708861ac5424a6616090
dfinzer <devin_finzer@brown.edu>
2012-07-21 15:16:59 -0700

    Style improvements and game model

--- public/js/character.js
+++ public/js/character.js
@@ -5,3 +5,7 @@ function Character() {
 Character.prototype = {
 
 };
+
+if (typeof exports != 'undefined') {
+  exports.Character = Character;
+}
--- public/js/wiki.js
+++ public/js/wiki.js
@@ -1,4 +1,5 @@
 var socket = io.connect('http://localhost:7000');
-socket.on('news', function (data) {
+socket.emit('getNode', { 'node_id': 'data' });
+socket.on('nodeData', function (data) {
   socket.emit('my other event', { my: 'data' });
 });
