b67b0cbe0a6782ede7207b02afaf4d745cf5068e
Tala Huhe <tala@fb.com>
2012-07-21 18:37:26 -0700

    Mouse over big

--- public/js/main.js
+++ public/js/main.js
@@ -16,8 +16,25 @@ $(document).ready(function() {
 
     var cubeMesh = GL.Mesh.cube();
 
-    gl.onupdate = function(seconds) {
+    var mousePosition = { x: 0, y: 0 };
+    
+    gl.onmousemove = function(e) {
+        mousePosition = { x: e.x, y: e.y };
+    }
 
+    gl.onupdate = function(seconds) {
+        var tracer = new GL.Raytracer();
+        var ray = tracer.getRayForPixel(mousePosition.x, mousePosition.y);
+        
+        for (var i = 0; i < relatedPages.length; i++) {
+            var page = relatedPages[i];
+            var result = GL.Raytracer.hitTestSphere(tracer.eye, ray, page.position, 1);
+            if (result) {
+                page.highlighted = true;
+            } else {
+                page.highlighted = false;
+            }
+        }
     };
 
     gl.ondraw = function() {
@@ -39,7 +56,7 @@ $(document).ready(function() {
         currentPage.draw();
 
         // related pages are further out
-        for (var i = 0; i < 3; i++) {
+        for (var i = 0; i < relatedPages.length; i++) {
             var page = relatedPages[i];
             page.draw();
         }
