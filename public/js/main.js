3258ef1d0cc4cd9b1ba5abf973324046cc7c41f9
Tala Huhe <tala@fb.com>
2012-07-22 02:46:56 -0700

    fixed flashing bug

--- public/js/main.js
+++ public/js/main.js
@@ -19,7 +19,13 @@ function useNewPages() {
     
     $.each(relatedPages, function(i, elt) {
         elt.alpha = 0;
-        elt.textElement.fadeIn(textFadeDuration * 1000);
+        elt.textElement.css("opacity", 0);
+        elt.textElement.animate({
+            opacity: 0.4,
+        }, textFadeDuration * 1000, function() {
+            // this is so that opacity is controlled via css
+            $(this).css("opacity", "");
+        });
         $("body").append(elt.textElement);
     });
 
@@ -91,8 +97,6 @@ $(document).ready(function() {
             } else {
                 // ???
             }
-
-            $(".page-title").fadeIn(textFadeDuration * 1000);
         }
     };
 
