dc63e486993310aa2ac1a0ceed0019b29ab68c93
Tala Huhe <tala@fb.com>
2012-07-22 07:54:23 -0700

    removing text fading

--- public/js/main.js
+++ public/js/main.js
@@ -20,6 +20,7 @@ function useNewPages() {
     $.each(relatedPages, function(i, elt) {
         elt.alpha = 0;
 
+        /*
         elt.textElement.css("opacity", "0.05");
         elt.textElement.animate({
             opacity: 0.4,
@@ -27,6 +28,7 @@ function useNewPages() {
             // this is so that opacity is controlled via css
             $(this).css("opacity", "");
         });
+        */
         $("body").append(elt.textElement);
     });
 
