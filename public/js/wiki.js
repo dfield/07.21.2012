98b1ee065fa5074b2e6186fd411acff5ea0ecd9c
dfinzer <devin_finzer@brown.edu>
2012-07-21 17:35:30 -0700

    Support for displaying a list of players

--- public/js/wiki.js
+++ public/js/wiki.js
@@ -1,10 +1,27 @@
+var players = {};
 var socket = io.connect('http://localhost:7000');
+
 socket.emit('getNode', { 'node_id': 'data' });
+
 socket.on('nodeData', function (data) {
   socket.emit('my other event', { my: 'data' });
 });
 
-socket.on('players')
+socket.on('players', function(playerData) {
+  players = playerData;
+  displayPlayers();
+});
+
+function displayPlayers() {
+  $("#players").html("");
+  console.log(players);
+  for (var playerId in players) {
+    var player = players[playerId];
+    var playerDiv = makeElement(".player");
+    playerDiv.text(player.name);
+    $("#players").append(playerDiv);
+  }
+}
 
 function login(loginData) {
   socket.emit('login', loginData)
