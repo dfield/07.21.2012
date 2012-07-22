7fb5160dee4bf54cc4571e6ebcb37ee27d8c9bae
dfinzer <devin_finzer@brown.edu>
2012-07-21 18:41:03 -0700

    Fixed a bug

--- game.js
+++ game.js
@@ -41,7 +41,8 @@ Monitor.prototype.diff = function() {
   */
 function Game() {
   this.clients = {};
-  this.articles = {1: new Article("Tala Huhe - Man of the Year"), 2: new Article("Devin Finzer - One cool dude")};
+  this.articles = {};
+  //this.articles = {1: new Article("Tala Huhe - Man of the Year"), 2: new Article("Devin Finzer - One cool dude")};
   this.players = {};
   this.nextPlayerId = 0;
 }
